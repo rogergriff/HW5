d797787e85891d1311e4f09082ee1e10be9c1b54
Hawkinsonb <brandon.hawkinson@icloud.com>
2016-04-28 17:31:40 -0700

    Update TheRestOfTheProject.cpp

--- TheRestOfTheProject.cpp
+++ TheRestOfTheProject.cpp
@@ -1,1 +1,11 @@
 
+
+//shape class is virtual as its default for our actual shapes
+class Shape {
+
+virtual ~Shape() = default; //destructs to default
+
+virtual bool contains(const Point & p); //virtual function to determine a point 
+
+}
+
