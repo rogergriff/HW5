ca4e95f1b229b5b58b5e692d1927f133af228330
rogergriff <rogergriff121@gmail.com>
2016-04-28 17:30:32 -0700

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -2,27 +2,28 @@
 using std::cout;
 using std::endl;
 
-class Point{
+class Point {
 private:
 	double x;
 	double y;
 
 public:
-	Point(double a, double b){
+	Point(double a, double b) {
 		x = a;
 		y = b;
 	}
-	Point(){
+	Point() {
 		x = 0;
 		y = 0;
 	}
-	void get(){
-		cout << "x = " << x << " y = " << y << endl;
+	Point get() {
+		Point a(x, y);
+		return a;
 	}
-	~Point(){}
+	~Point() {}
 };
 
-int main(){
+int main() {
 	Point a(2.2, 4.4);
 	Point b;
 	cout << "a" << endl;
