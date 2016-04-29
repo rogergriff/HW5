9d9714487f891007db8260d970bbe2f2e3538078
rogergriff <rogergriff121@gmail.com>
2016-04-28 17:32:48 -0700

    Update TheRestOfTheProject.cpp

--- TheRestOfTheProject.cpp
+++ TheRestOfTheProject.cpp
@@ -1,5 +1,39 @@
-
-
+#inlude<iostream>
+ using std::cout;
+  using std::endl;
+  
+ -class Point{
+ +class Point {
+  private:
+  	double x;
+  	double y;
+  
+  public:
+ -	Point(double a, double b){
+ +	Point(double a, double b) {
+  		x = a;
+  		y = b;
+  	}
+ -	Point(){
+ +	Point() {
+  		x = 0;
+  		y = 0;
+  	}
+ -	void get(){
+ -		cout << "x = " << x << " y = " << y << endl;
+ +	Point get() {
+ +		Point a(x, y);
+ +		return a;
+  	}
+ -	~Point(){}
+ +	~Point() {}
+  };
+  
+ -int main(){
+ +int main() {
+  	Point a(2.2, 4.4);
+  	Point b;
+  	cout << "a" << endl;
 //shape class is virtual as its default for our actual shapes
 class Shape {
 
